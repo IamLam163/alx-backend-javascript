810ddfcdbbe915b73f6b5747f34a430c0cf1a6ec
IamLam163 <olamidealiyu@gmail.com>
2023-03-21 22:43:42 +0100

    thenables

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => ({ status: 200, body: 'success' }))
-    .catch((error) => new Error())
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
 const promise = Promise.resolve();
