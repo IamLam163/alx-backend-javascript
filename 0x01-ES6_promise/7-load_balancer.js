548a83734976d3ce9d9aba0bf11fe620fb9c802c
IamLam163 <olamidealiyu@gmail.com>
2023-03-22 18:04:43 +0100

    using the Promise.race()

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,24 +1,3 @@
 export default function loadBalancer(chinaDownload, USDownload) {
   return Promise.race([chinaDownload, USDownload]);
 }
-const ukSuccess = 'Downloading from UK is faster';
-const frSuccess = 'Downloading from FR is faster';
-
-const promiuseUK = new Promise(function(resolve, reject) {
-  setTimeout(resolve, 100, ukSuccess);
-});
-
-const promiseUKSlow = new Promise(function(resolve, reject) {
-  setTimeout(resolve, 400, ukSuccess);
-});
-
-const promiseFR = new Promise(function(resolve, reject) {
-  setTimeout(resolve, 200, frSuccess);
-});
-
-const test = async () => {
-  console.log(await loadBalancer(promiuseUK, promiseFR));
-  console.log(await loadBalancer(promiseUKSlow, promiseFR));
-}
-
-test();
