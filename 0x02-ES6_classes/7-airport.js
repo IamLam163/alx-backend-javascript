c5fcd8092aff4d19f0f4318f5bd09748c4a210ff
IamLam163 <olamidealiyu@gmail.com>
2023-03-25 04:24:43 +0100

    Class Airport

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,31 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('name must be a string');
+    } this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+    this._name = value;
+  }
+
+  get [Symbol.toStringTag]() {
+    return `${this._code}`;
+  }
+}
