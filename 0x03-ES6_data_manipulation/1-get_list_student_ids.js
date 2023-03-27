c83a124eba2f070ee6aa195791e0ca9236909634
IamLam163 <olamidealiyu@gmail.com>
2023-03-27 17:43:44 +0100

    using maps()

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+import getListStudents from './0-get_list_students';
+
+export default function getListStudentIds(getListStudents) {
+  if (!Array.isArray(getListStudents)) {
+    return [];
+  }
+  return getListStudents.map((list) => list.location);
+}
+
+console.log(getListStudentIds('hello'));
+console.log(getListStudentIds(getListStudents()));
