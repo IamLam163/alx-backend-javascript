3d893aa1f2fe73b63426b59b6d356f86c704dc7d
IamLam163 <olamidealiyu@gmail.com>
2023-03-29 05:54:51 +0100

    fixed bug

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,22 @@
 export default function cleanSet(set, startString) {
-  const filteredSet = new Set([...set].filter((value) => value.startsWith(startString)));
-  const cleanedArray = [...filteredSet].map((value) => value.replace(startString, ''));
-  return cleanedArray.join('-');
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  const filteredSet = [...set].filter((value) => {
+    if (typeof value !== 'string') {
+      return false;
+    }
+    if (!value.startsWith(startString)) {
+      return false;
+    }
+    const valueSubStr = value.substring(startString.length);
+    if (valueSubStr && valueSubStr !== value) {
+      parts.push(valueSubStr);
+    }
+    return true;
+  });
+  return parts.join('-');
 }
 
 console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
