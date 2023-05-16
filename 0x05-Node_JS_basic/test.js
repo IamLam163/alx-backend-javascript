e6ce75a0f7994ef8fad23c7f7224be0b1cb07275
IamLam163 <olamidealiyu@gmail.com>
2023-05-16 15:17:09 +0100

    first test

--- 0x05-Node_JS_basic/test.js
+++ 0x05-Node_JS_basic/test.js
@@ -1,13 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
+console.log('Welcome to Holberton School, what is your name?');
 
-  if (chunk) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+// let userInput = '';
+rl.on('line', function(input) {
+  console.log('Your name is: ', input);
+  console.log('This important software is now closing');
+  rl.close();
 });
