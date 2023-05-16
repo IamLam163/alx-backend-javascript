c63f820a465b0c36385e991837a66c2843f754a4
IamLam163 <olamidealiyu@gmail.com>
2023-05-16 01:50:20 +0100

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -11,31 +11,3 @@ process.stdin.on('readable', () => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-/*
- const readline = require('readline');
-
-console.log('Welcome to Holberton School, what is your name?');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-// let userInput = '';
-rl.on('line', function(input) {
-  console.log('Your name is: ', input);
-  console.log('This important software is now closing');
-  rl.close();
-});
-*/
-/*
-rl.question('What is your name\n', function(string) {
-  userInput = string;
-
-  console.log('Your name is ' + userInput);
-  if (userInput === 'exit') {
-    console.log('This important software is now closing');
-    rl.close();
-  }
-})*/
